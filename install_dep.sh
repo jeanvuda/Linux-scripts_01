1b91165d425660875e9eb5f67bc653e682007168
jeanvuda <johnavida@gmail.com>
2015-12-13 16:51:23 -0700

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 # Author: Jean Vuda
 
+# Install SSH handler
 sudo apt-get update
 sudo apt-get install openssh-server
 sudo ssh -p 3022 osboxes@127.0.0.1
+
+# Install Java
+sudo apt-add-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+
+# Install Pycharm
+mkdir -p ~/opt/packages/pycharm
+cd ~/opt/packages/pycharm
+wget http://download.jetbrains.com/python/pycharm-community-4.0.4.tar.gz
+gzip -dc pycharm-5.0.2.tar.gz | tar xf -
+ln -s ~/opt/packages/pycharm/pycharm-community-4.0.4 ~/opt/pycharm
+
+# Start PyCharm
+~/opt/pycharm/bin/pycharm.sh
