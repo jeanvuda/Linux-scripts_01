f5f916afb416f7c188b163686aa8c06825872a6a
jeanvuda <johnavida@gmail.com>
2015-12-13 16:56:50 -0700

    Update install_dep.sh

--- install_dep.sh
+++ install_dep.sh
@@ -15,8 +15,8 @@ sudo apt-get install oracle-java8-installer
 mkdir -p ~/opt/packages/pycharm
 cd ~/opt/packages/pycharm
 wget https://download.jetbrains.com/python/pycharm-community-5.0.2.tar.gz
-gzip -dc pycharm-5.0.2.tar.gz | tar xf -
-ln -s ~/opt/packages/pycharm/pycharm-community-4.0.4 ~/opt/pycharm
+gzip -dc pycharm-community-5.0.2.tar.gz | tar xf -
+ln -s ~/opt/packages/pycharm/pycharm-community-5.0.2 ~/opt/pycharm
 
 # Start PyCharm
 ~/opt/pycharm/bin/pycharm.sh
