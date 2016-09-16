c7f3814cd6a291addd5f8b0edcde0ba6f60f39d7
John <johnavida@gmail.com>
2016-09-15 21:25:29 -0600

    Update Install anaconda.sh

--- Install anaconda.sh
+++ Install anaconda.sh
@@ -5,3 +5,11 @@ wget https://repo.continuum.io/archive/Anaconda3-4.1.1-Linux-x86_64.sh
 # Give the location where you downloaded
 bash '/home/john/Anaconda3-4.1.1-Linux-x86_64.sh'
 
+# Install pip and jupyter
+sudo apt install python3-pip
+pip3 install --upgrade pip
+pip3 install jupyter
+
+# Initiate jupyter notebook
+jupyter notebook
+
